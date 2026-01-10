3253499cb177cd4bc895fc86892f2dee43033a82
kamlesh.stech <kamlesh.stech@gmail.com>
2026-01-10 17:55:33 +0530

    API-Gateway

---
+++ API-Gateway/src/main/java/com/practice/gateway/filter/LoggingGlobalFilter.java
@@ -0,0 +1,26 @@
+package com.practice.gateway.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggingGlobalFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		System.out.println("Global Filter Hit for Every Req : "+req.getRequestURL());
+		
+		chain.doFilter(request, response); 
+	}
+	
+}
